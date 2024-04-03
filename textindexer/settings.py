360dff8c6eef537348f5813be8198db2f8d5851c
Gaurav241 <gaurav.govinda2420@gmail.com>
2024-04-03 21:40:35 +0530

    TextIndexer: Switch to postgresSQL.

--- textindexer/settings.py
+++ textindexer/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'textindexer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'codemonk',
+        'USER': 'postgres',
+        'PASSWORD': '2411',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
